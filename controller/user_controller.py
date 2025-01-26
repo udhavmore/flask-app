28a16a3b3535183a75c8d3f6aec2eda607cfa0af
udhavmore <udhav.tauras04@gmail.com>
2025-01-26 23:48:28 +0530

    Changes:
    - Added create user API endpoint and controller funtion
    - Code refactor

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,8 +1,13 @@
 from app import app
 from model.User import User
+from flask import request
 
 user = User()
 
 @app.route('/user/getall')
 def getallusers():
     return user.user_getall()
+
+@app.route('/user/addone', methods=['POST'])
+def addoneuser():
+    return user.user_add_one(request.form)
--- model/User.py
+++ model/User.py
@@ -7,15 +7,23 @@ class User:
             # Establising MySQL database connection
             self.con = mysql.connector.connect(host='localhost',username='root',password='root',database='flask-app')
             self.cur = self.con.cursor(dictionary=True) # Get the result in dictionary format
+            self.con.autocommit = True  # Setting autocommit=True to automatically commit the changes into database
             print("Connection Successful!!!")
         except Exception as e:
             print(f"Error while connecting to database: {e}")
 
     def user_getall(self):
         """Get all the users from database"""
-        self.cur.execute("SELECT * from user")
+        self.cur.execute("SELECT * FROM user")
         result = self.cur.fetchall()
         if len(result) > 0:
             return json.dumps(result)
         else:
             return "No Data Found!!!"
+    
+    def user_add_one(self, data):
+        """Add a users into the database"""
+        query = f"INSERT INTO user(name, email, phone, role, password) VALUES('{data['name']}', '{data['email']}', '{data['phone']}', '{data['role']}', '{data['password']}')"
+        print(query)
+        self.cur.execute(query)
+        return "User Created Successfully!!!"
