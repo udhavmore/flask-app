154f7698d2ec0baad69c712538ecde01c775efa1
udhavmore <udhav.tauras04@gmail.com>
2025-01-29 19:38:30 +0530

    Changes:
    - Added patch user API endpoint and controller function
    - Refactored the code

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -8,14 +8,18 @@ user = User()
 def getallusers():
     return user.user_getall()
 
-@app.route('/user/addone', methods=['POST'])
+@app.route('/user/add', methods=['POST'])
 def addoneuser():
-    return user.user_add_one(request.form)
+    return user.user_add(request.form)
 
-@app.route('/user/updateone', methods=['PUT'])
+@app.route('/user/update', methods=['PUT'])
 def updateuser():
-    return user.user_update_one(request.form)
+    return user.user_update(request.form)
 
-@app.route('/user/deleteone/<id>', methods=['DELETE'])
+@app.route('/user/delete/<id>', methods=['DELETE'])
 def deleteuser(id):
-    return user.user_delete_one(id)
+    return user.user_delete(id)
+
+@app.route('/user/patch/<id>', methods=['PATCH'])
+def patchuser(id):
+    return user.user_patch(request.form, id)
--- model/User.py
+++ model/User.py
@@ -24,14 +24,14 @@ class User:
         else:
             return make_response({"message": "No Data Found!!!"}, 204)
     
-    def user_add_one(self, data):
+    def user_add(self, data):
         """Add a users into the database"""
         query = f"INSERT INTO user(name, email, phone, role, password) VALUES('{data['name']}', '{data['email']}', '{data['phone']}', '{data['role']}', '{data['password']}')"
         print(query)
         self.cur.execute(query)
         return make_response({"message":"User Created Successfully!!!"}, 201)
     
-    def user_update_one(self, data):
+    def user_update(self, data):
         """Update entry for user in database"""
         query = f"UPDATE user SET name='{data['name']}', email='{data['email']}', phone='{data['phone']}', role='{data['role']}', password='{data['password']}' WHERE id={data['id']}"
         print(query)
@@ -41,7 +41,7 @@ class User:
         else:
             return make_response({"message": "Nothing to Update!!!"}, 202)
     
-    def user_delete_one(self, id):
+    def user_delete(self, id):
         """Delete user entry from database"""
         query = f"DELETE FROM user WHERE id={id}"
         print(query)
@@ -50,3 +50,16 @@ class User:
             return make_response({"message": "User deleted Successfully!!!"},200)
         else:
             return make_response({"message": "Nothing to Delete!!!"}, 202)
+    
+    def user_patch(self, data, id):
+        """Update a field for user in database"""
+        query = "UPDATE user SET "
+        for key in data:
+            query += f"{key}='{data[key]}',"
+        query = query[:-1] + f" WHERE id={id}"
+        print(query)
+        self.cur.execute(query)
+        if self.cur.rowcount>0:
+            return make_response({"message": "User updated Successfully!!!"}, 201)
+        else:
+            return make_response({"message": "Nothing to Update!!!"}, 202)
