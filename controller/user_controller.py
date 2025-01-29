472295c91251e321f78122608de8aff0224a493e
udhavmore <udhav.tauras04@gmail.com>
2025-01-30 01:20:03 +0530

    Changes:
    - Added upload user avatar API endpoint and controller function
    - Added API endpoint to get the user avatar

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,6 +1,7 @@
 from app import app
 from model.User import User
 from flask import request
+from datetime import datetime
 
 user = User()
 
@@ -27,3 +28,18 @@ def patchuser(id):
 @app.route('/user/getall/limit/<limit>/page/<page>')
 def userpagination(limit, page):
     return user.user_pagination(limit, page)
+
+@app.route('/user/<id>/upload/avatar', methods=['PUT'])
+def uploaduseravatar(id):
+    file = request.files['avatar']
+    time_now = str(datetime.now().timestamp()).replace(".", "")
+    file_name_split = file.filename.split(".")
+    file_ext = file_name_split[len(file_name_split)-1]
+    unique_filename = time_now + "." + file_ext
+    file_full_path = f"uploads/{unique_filename}"
+    file.save(file_full_path)
+    return user.user_upload_avatar(id, file_full_path)
+
+@app.route('/uploads/<filename>')
+def getuseravatar(filename):
+    return user.user_avatar(filename)
--- model/User.py
+++ model/User.py
@@ -1,6 +1,6 @@
 import mysql.connector
 import json
-from flask import make_response
+from flask import make_response, send_file
 
 class User:
     def __init__(self):
@@ -77,3 +77,17 @@ class User:
             return res
         else:
             return make_response({"message": "No Data Found!!!"}, 204)
+    
+    def user_upload_avatar(self, id, file_path):
+        """Upload avatar for user and store the URL for avatar into the database"""
+        query = f"UPDATE user SET avatar='{file_path}' WHERE id={id}"
+        print(query)
+        self.cur.execute(query)
+        if self.cur.rowcount>0:
+            return make_response({"message": "File uploaded successfully!!!"}, 201)
+        else:
+            return make_response({"message": "Nothing to Update!!!"}, 202)
+    
+    def user_avatar(self, filename):
+        """Show user avatar"""
+        return send_file(f"uploads/{filename}")
